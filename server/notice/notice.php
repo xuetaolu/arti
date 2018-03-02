be35317729bba07f28b5aa52aa83512f388f0753
xuetaolu <724478775@qq.com>
2018-03-02 16:50:46 +0800

    modified:   server/notice/notice.php

--- server/notice/notice.php
+++ server/notice/notice.php
@@ -1,26 +1,34 @@
 <?php
- $startRow=$_GET["startRow"] ;//接收参数
- $endRow=$_GET["endRow"];
- 
- header("charset=utf-8");
+ $start=$_GET["start"] ;//index = 20
+ $len=$_GET["len"];     //  len = 10   // index: 20 ~ 29 
+ 									   // MySQL: 21 ~ 30
+ if($start >= 0 && $len > 0) {
+ 	 $firstPick = $start + $len;
+	 $secondPick = $len;
 
- $conn = mysqli_connect("localhost", "root", "jnuzhxy-1s", "zhxy", "3333");//连接MYSQL数据库
- 
-if (!$conn) 
-{ 
-    die("连接错误: " . $conn->connect_error); 
-} 
+	 header("charset=utf-8");
 
- $sql = "SELECT * FROM news";//响应请求
- 
- $result = $conn->query($sql);
+	 $conn = mysqli_connect("172.16.0.12", "root", "jnuzhxy-1s", "zhxy", "3333");//连接MYSQL数据库
+	 
+	if (!$conn) 
+	{ 
+	    die("连接错误: " . $conn->connect_error); 
+	}
 
- if ($result->num_rows > 0) {// 输出数据
- 
-    while($row = $result->fetch_assoc()) {
- 
-        echo json_encode($row);//将请求结果转换为json格式
- 
-    }
-}
+	 $sql = "SELECT * FROM (SELECT TOP $secondPick * FROM (SELECT TOP $firstPick * FROM news order by id desc) order by id desc) order by id desc";//响应请求
+	 
+	 $result = $conn->query($sql);
+
+	 if ($result->num_rows > 0) {// 输出数据
+	 
+	    while($row = $result->fetch_assoc()) {
+	 
+	        echo json_encode($row);//将请求结果转换为json格式
+	 
+	    }
+	 }
+	 else {
+	 	echo '';
+	 }
+ }
 ?>
